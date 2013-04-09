0678c1c20a61f09bc89ff662d082a293b46097c0
Jimima <jimmy_james@fastmail.co.uk>
2013-04-09 11:00:34 +0000

    Latest version

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function compile(str, path) {
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
+app.use(express.bodyParser())
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
@@ -18,8 +19,38 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+var white = 0;
+var brown = 0;
+var name = 'Brandwatch Table Football Scoreboard!';
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : name,
+    score1 : white,
+    score2 : brown }
+  )
+})
+
+app.get('/white', function(req, res){
+  white++;
+  res.send('White: ' + white + " Brown: " + brown);
+});
+
+
+app.get('/score', function(req, res){
+  res.send('White: ' + white + " Brown: " + brown);
+});
+
+
+app.get('/brown', function(req, res){
+  brown++;
+  res.send('White: ' + white + " Brown: " + brown);
+});
+
+app.get('/reset', function(req, res){
+  white = 0;
+  brown = 0;
+  res.send('White: ' + white + " Brown: " + brown);
 });
 
 app.listen(3000);
